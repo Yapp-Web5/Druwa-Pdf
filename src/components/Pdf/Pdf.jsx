65dd1844b76dbaa8545684ca6ce6a352785079ad
AnGwangHo <akh4444@nate.com>
2018-09-29 01:35:20 +0900

    Pdf.jsx, scss : router로 pdf page 이동, 임시 select text 기능 구현, layout basic 구분 See : #11 PDF 파일 렌더링

--- src/components/Pdf/Pdf.jsx
+++ src/components/Pdf/Pdf.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { Document, Page } from "react-pdf";
+import * as styles from "./Pdf.scss";
+import { Link } from "react-router-dom";
 
 class Pdf extends Component {
   state = {
     numPages: null,
-    pageNumber: 1,
+    pageNumber: 1
   };
 
   onDocumentLoad = ({ numPages }) => {
@@ -12,42 +14,54 @@ class Pdf extends Component {
   };
 
   handlePrev = () => {
+    if (this.state.pageNumber <= 1) return;
+
     this.setState({ pageNumber: this.state.pageNumber - 1 });
   };
 
   handleNext = () => {
+    if (this.state.pageNumber >= this.state.numPages) return;
     this.setState({ pageNumber: this.state.pageNumber + 1 });
+  };
+
+  handleOnSelect = event => {
+    event.stopPropagation();
+    const select_text = window.getSelection().toString();
+    console.log("select된 text[" + select_text + "]");
   };
 
   render() {
     const { pageNumber, numPages } = this.state;
     return (
       <div>
-        <div name="pdf_view">
+        <div
+          className={styles.pdf_view}
+          name="pdf_view"
+          onMouseUp={this.handleOnSelect}
+          onMouseDown={this.test}
+        >
           <Document
             file="https://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf"
             onLoadSuccess={this.onDocumentLoad}
           >
-            <Page pageNumber={pageNumber} />
+            <Page pageNumber={pageNumber} width={500} />
           </Document>
         </div>
-        <div name="information_rect">
-          <input value="제목" />
+        <div className={styles.information_rect} name="information_rect">
+          <input />
           <div type="text">By 작성자</div>
           <div type="text">Updated September 2, 2018</div>
         </div>
-        <div name="pdf_moverect">
-          <li onClick={this.handlePrev}>
-            <a href="#">Prev</a>
-          </li>
-          <p>
-            Page {pageNumber}
-            of
-            {numPages}
-          </p>
-          <li onClick={this.handleNext}>
-            <a href="#">Next</a>
-          </li>
+        <div className={styles.pdf_moverect} name="pdf_moverect">
+          <div className={styles.left} onClick={this.handlePrev}>
+            <Link to="/create">Prev</Link>
+          </div>
+          <div className={styles.left}>
+            {pageNumber}/{numPages}
+          </div>
+          <div className={styles.left} onClick={this.handleNext}>
+            <Link to="/create">Next</Link>
+          </div>
           <button>링크</button>
           <button>전체화면</button>
         </div>
